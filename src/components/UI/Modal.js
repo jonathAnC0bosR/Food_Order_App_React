5de4e966e8c7f2a0039156725d9c5074b598e290
Jonathan Cobos <jonathan.cobos@outlook.es>
2023-08-05 16:38:55 -0600

    Added Cart component and styles form input

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,30 @@
+import ReactDOM from "react-dom";
+import styles from "./Modal.module.css";
+
+const Backrop = (props) => {
+  return <div className={styles.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={styles.modal}>
+      <div className={styles.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <>
+      {ReactDOM.createPortal(<Backrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </>
+  );
+};
+
+export default Modal;
