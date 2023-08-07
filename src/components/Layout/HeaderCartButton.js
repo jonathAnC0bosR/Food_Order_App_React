0b86bcedbd10251353717db687e550b28c5d8ea4
Jonathan Cobos <jonathan.cobos@outlook.es>
2023-08-07 13:32:32 -0600

    Added an animation to add or remove items

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,27 +1,45 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from "react";
 
-import styles from './HeaderCartButton.module.css';
-import CartContext from '../../context/cartContext';
-import CartIcon from '../Cart/CartIcon';
+import styles from "./HeaderCartButton.module.css";
+import CartContext from "../../context/cartContext";
+import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = (props) => {
-    const cartCtx = useContext(CartContext);
+  const [btnIsHighlated, setBtnIsHighlahted] = useState(false);
+  const cartCtx = useContext(CartContext);
 
-    const numberOfCartItems = cartCtx.items.reduce((currNum, item) => {
-        return currNum + item.amount;
-    }, 0)
+  const numberOfCartItems = cartCtx.items.reduce((currNum, item) => {
+    return currNum + item.amount;
+  }, 0);
 
-    return (
-        <button className={styles.button} onClick={props.onClick}>
-            <span className={styles.icon}>
-                <CartIcon />
-            </span>
-            <span>Your Cart</span>
-            <span  className={styles.badge}>
-                {numberOfCartItems}
-            </span>
-        </button>
-    )
-}
+  const btnClasses = `${styles.button} ${btnIsHighlated ? styles.bump : ""}`;
+
+  const { items } = cartCtx;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlahted(true);
+    const timer = setTimeout(() => {
+      setBtnIsHighlahted(false);
+    }, 300);
+
+    return () => {
+        clearTimeout(timer)
+    }
+
+  }, [items]);
+
+  return (
+    <button className={btnClasses} onClick={props.onClick}>
+      <span className={styles.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={styles.badge}>{numberOfCartItems}</span>
+    </button>
+  );
+};
 
 export default HeaderCartButton;
